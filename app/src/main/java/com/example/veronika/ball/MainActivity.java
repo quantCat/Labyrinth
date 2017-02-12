ff220289fb7884b03c13a7b887c71c1c3b4c8273
Quant Cat <quantCat@github.com>
2017-02-12 13:19:13 +0200

    Initial empty

---
+++ app/src/main/java/com/example/veronika/ball/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.veronika.ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
