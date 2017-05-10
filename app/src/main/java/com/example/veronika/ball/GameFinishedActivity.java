313fb7b136660c64772509b58d724bc72e1fa8aa
Quant Cat <quantCat@github.com>
2017-05-10 21:16:03 +0300

    WIP in MapChooser and GameFinished.

--- app/src/main/java/com/example/veronika/ball/GameFinishedActivity.java
+++ app/src/main/java/com/example/veronika/ball/GameFinishedActivity.java
@@ -1,8 +1,14 @@
 package com.example.veronika.ball;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by veronika on 23/04/2017.
@@ -12,6 +18,37 @@ public class GameFinishedActivity extends AppCompatActivity {
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.game_finished_activity);
+        Intent intent = getIntent();
+        String game_status = intent.getStringExtra("STATUS");
+        final String level = intent.getStringExtra("LEVEL");
+        int stars = intent.getIntExtra("STARS", 0);
+        TextView gr = (TextView) findViewById(R.id.game_result);
+        if (game_status.equals("WIN")) {
+            gr.setText(String.format("You win!\n" +
+                    "Your result saved. Collected stars: %d", stars));
+        } else if (game_status.equals("LOSE")) {
+            gr.setText("You lose...");
+        } else {
+            throw new RuntimeException("Wrong intent extra");
+        }
+
+
+        final Intent menu_intent = new Intent(this, StartActivity.class);
+        final Button bm = (Button) findViewById(R.id.tmn_btn);
+        bm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(menu_intent);
+            }
+        });
+
+        final Intent game_intent = new Intent(this, GameActivity.class);
+        final Button bsg = (Button) findViewById(R.id.rst_btn);
+        bsg.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                game_intent.putExtra("LEVEL", level);
+                startActivity(game_intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/veronika/ball/MapChooseActivity.java
+++ app/src/main/java/com/example/veronika/ball/MapChooseActivity.java
@@ -1,13 +1,16 @@
 package com.example.veronika.ball;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapChooseActivity extends Activity {
 
@@ -31,9 +34,53 @@ public class MapChooseActivity extends Activity {
         RadioGroup radios = (RadioGroup)findViewById(R.id.maplist);
     }
 
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This means the button "back" is pressed on screen
+            if (!super.onMenuItemSelected(featureId, item)) {
+                NavUtils.navigateUpFromSameTask(this);
+            }
+            return true;
+        }
+
+        /////
+        Map<String, Integer> mapIDList = getMapIdList();
+        RadioButton rb = (RadioButton) findViewById(id);
+        int mapId = mapIDList.get(rb.getText());
+        Intent game_intent = new Intent(this, GameActivity.class);
+        game_intent.putExtra("MAP", mapId);
+        startActivity(game_intent);
+
+        return super.onMenuItemSelected(featureId, item);
+    }
+
     public Map<String, String> getMapList() {
-        Map r = new HashMap<>();
-        r.put("Map", "map");
+        Map r = new TreeMap<>();
+        r.put("Map0_tutorial", "map0_tutorial");
+        r.put("Map1", "map1");
+        r.put("Map2", "map2");
+        r.put("Map3", "map3");
+        r.put("Map4", "map4");
+        r.put("Map5", "map5");
+        r.put("Map6", "map6");
+        r.put("Map7", "map7");
+        r.put("Map_tester", "map");
+        return r;
+    }
+
+    public Map<String, Integer> getMapIdList() {
+        Map r = new TreeMap<>();
+        r.put("Map0_tutorial", R.raw.map0_tutorial);
+        r.put("Map1", R.raw.map1);
+        r.put("Map2", R.raw.map2);
+        r.put("Map3", R.raw.map3);
+        r.put("Map4", R.raw.map4);
+        r.put("Map5", R.raw.map5);
+        r.put("Map6", R.raw.map6);
+        r.put("Map7", R.raw.map7);
+        r.put("Map_tester", R.raw.map);
         return r;
     }
 }
